86af06f39d423403e865fa912a56f86f63271332
theodorebaston <theodorebaston@gmail.com>
2024-12-16 13:00:32 -0500

    added comparison of lists to judge total distance between them

--- day1/day1.js
+++ day1/day1.js
@@ -3,11 +3,33 @@ const secondList = [4,3,5,3,9,3];
 
 function Sorter(listOne, listTwo) {
     
+    // Sort the arrays from lowest to highest
     listOne.sort((a,b) => a - b);
     listTwo.sort((a,b) => a - b);
 
     console.log(firstList);
     console.log(secondList);
+
+    // Safeguard against arrays of different lengths, just in case.
+    var arrayLength;
+    if(listOne.length >= listTwo.length) {
+        arrayLength = listOne.length
+    } else if (listOne.length < listTwo.length) {
+        arrayLength = listTwo.length
+    }
+
+    // Add up the difference in each array position between the two lists
+    var totalDistance = 0;
+    for (let i = 0; i < arrayLength; i++) {
+        var diff = listOne[i] - listTwo[i]
+        if(diff < 0) {
+            totalDistance += (diff * -1)
+        } else {
+            totalDistance += diff
+        }
+    }
+
+    console.log(totalDistance)
 }
 
 Sorter(firstList, secondList);
